028a00d46ed09712d341c8718ebc9e49919231bb
Allupeng <allupeng417@gmail.com>
2022-04-18 20:40:14 +0800

    init Projects

---
+++ algorithms/go/1.two-sum.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		// map return value,ok. we use _ to ignore the value
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
+
+// @lc code=end
+
