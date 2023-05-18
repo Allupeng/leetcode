7db67b16accfb242f407a6e69550b80bbc289136
allupeng <allupeng417@gmail.com>
2023-05-18 20:43:58 +0800

    java 1.two_sum.java

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # leetcode
-leetcode代码记录
+leetcode 刷题记录
+
+# List
+
+
+# 注意事项
+1. 文件夹结构参考 hao.chen leetcode刷题记录
+2. 题目代码命名格式为 题目序号 + "_" + 题目名驼峰命名法
---
+++ algorithms/java/0001_two_sum.java
@@ -0,0 +1,18 @@
+/**
+ * find two value sum = target
+ * if find
+ * return two nums index
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap();
+        for (int i = 0; i < nums.length; i++){
+            int another = target - nums[i];
+            if (numMap.get(another) != null){
+                return new int[]{numMap.get(another), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
