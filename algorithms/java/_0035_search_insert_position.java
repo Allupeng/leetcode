4cf9fbad288b83f8fe0b97cb49f17e5aead52d67
allupeng <allupeng417@gmail.com>
2023-05-24 19:15:23 +0800

    finish 35- search_insert_position

--- algorithms/java/_0035_search_insert_position.java
+++ algorithms/java/_0035_search_insert_position.java
@@ -57,9 +57,27 @@
 public class _0035_search_insert_position {
     class Solution {
         public int searchInsert(int[] nums, int target) {
-            int left = 0;
-            int right = nums.length - 1;
-
+            int lo = 0;
+            int hi = nums.length - 1;
+            while (lo <= hi){
+                int mid = lo + ((hi - lo) >> 1);
+                if(target == nums[mid]){
+                    return mid;
+                }else if(nums[mid] > target){
+                    if(mid == 0 || nums[mid - 1] < target){
+                        return mid;
+                    }else{
+                        hi = mid - 1;
+                    }
+                }else{
+                    if(mid == nums.length - 1 || nums[mid + 1] > target){
+                        return mid + 1;
+                    }else{
+                        lo = mid + 1;
+                    }
+                }
+            }
+            return -1;
         }
     }
 }
